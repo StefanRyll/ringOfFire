03d6f6688c7a4a205c92e3821b80fb601fe89ede
StefanRyll <142165351+StefanRyll@users.noreply.github.com>
2024-03-24 18:16:52 +0100

    update forloop cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../modules/game';
 
 @Component({
   selector: 'app-game',
@@ -10,6 +11,12 @@ import { Component } from '@angular/core';
 })
 export class GameComponent {
   pickCardAnimation = false;
+  game: Game;
+
+  constructor() {
+    this.game = new Game();
+    console.log(this.game);
+  }
 
   takeCard() {
     this.pickCardAnimation = true;
